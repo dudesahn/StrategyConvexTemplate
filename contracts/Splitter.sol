7ee8f7f348fa78e2385795573a54d454625e326d
wavey0x <retrowaveyy@gmail.com>
2022-08-09 23:06:41 -0400

    feat: review 1

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -55,13 +55,13 @@ contract Splitter {
         uint userSlope;
     }
 
-    uint constant precision = 10_000;
-    uint constant WEEK = 86400 * 7;
-    IERC20 constant crv = IERC20(0xD533a949740bb3306d119CC777fa900bA034cd52);
-    IYveCRV constant yvecrv = IYveCRV(0xc5bDdf9843308380375a611c18B50Fb9341f502A);
-    IERC20 constant liquidityPool = IERC20(0xdaDfD00A2bBEb1abc4936b1644a3033e1B653228);
-    IGauge constant gaugeController = IGauge(0x2F50D538606Fa9EDD2B11E2446BEb18C9D5846bB);
-    address constant gauge = 0x8f162742a7BCDb87EB52d83c687E43356055a68B;
+    uint internal constant precision = 10_000;
+    uint internal constant WEEK = 7 days;
+    IERC20 internal constant crv = IERC20(0xD533a949740bb3306d119CC777fa900bA034cd52);
+    IYveCRV internal constant yvecrv = IYveCRV(0xc5bDdf9843308380375a611c18B50Fb9341f502A);
+    IERC20 public constant liquidityPool = IERC20(0xdaDfD00A2bBEb1abc4936b1644a3033e1B653228);
+    IGauge public constant gaugeController = IGauge(0x2F50D538606Fa9EDD2B11E2446BEb18C9D5846bB);
+    address public constant gauge = 0x8f162742a7BCDb87EB52d83c687E43356055a68B;
     mapping(address => uint) pendingShare; 
     
     Yearn yearn;
@@ -75,8 +75,8 @@ contract Splitter {
             address(0xFEB4acf3df3cDEA7399794D0869ef76A6EfAff52), // recipient
             address(0xF147b8125d2ef93FB6965Db97D6746952a133934), // voter
             address(0xFEB4acf3df3cDEA7399794D0869ef76A6EfAff52), // admin
-            7_000, // profit factor (terms)
-            5_000 // Yearn discretionary % of CRV to lock as veCRV on each split
+            7_000, // share of profit (initial terms of deal)
+            5_000 // Yearn's discretionary % of CRV to lock as veCRV on each split
         );
     }
 
@@ -103,13 +103,13 @@ contract Splitter {
             emit Split(0, 0, crvBalance, period.period);
             return;
         }
-        uint _precision = precision;
-        uint yearnAmount = crvBalance * yRatio / _precision;
-        uint templeAmount = crvBalance * tRatio / _precision;
-        uint keep = yearnAmount * yearn.keepCRV / _precision;
+        uint yearnAmount = crvBalance * yRatio / precision;
+        uint templeAmount = crvBalance * tRatio / precision;
+        uint keep = yearnAmount * yearn.keepCRV / precision;
         if (keep > 0) {
             crv.transferFrom(strategy, address(this), keep);
             yvecrv.deposit(keep);
+            IERC20(address(yvecrv)).transfer(yearn.recipient, keep);
         }
         crv.transferFrom(strategy, yearn.recipient, yearnAmount - keep);
         crv.transferFrom(strategy, templeRecipient, templeAmount);
@@ -129,25 +129,24 @@ contract Splitter {
     }
 
     function _computeSplitRatios() internal view returns (uint yRatio, uint tRatio) {
-        uint _precision = precision; // @dev Move value into memory
         uint userSlope = period.userSlope;
-        if(period.userSlope == 0) return (0, 10_000);
+        if(userSlope == 0) return (0, 10_000);
         uint relativeSlope = userSlope * precision / period.globalSlope;
         uint lpSupply = liquidityPool.totalSupply();
         if (lpSupply == 0) return (10_000, 0); // @dev avoid div by 0
         uint gaugeDominance = 
             IStrategy(strategy).estimatedTotalAssets() 
-            * _precision 
+            * precision 
             / lpSupply;
         if (gaugeDominance == 0) return (10_000, 0); // @dev avoid div by 0
         yRatio = relativeSlope 
-            * _precision 
+            * precision 
             / gaugeDominance;
         // Should not return > 100%
         if (yRatio > 10_000){
             return (10_000, 0);
         }
-        tRatio = _precision - yRatio;
+        tRatio = precision - yRatio;
     }
 
     // @dev Estimate only. 
@@ -160,7 +159,6 @@ contract Splitter {
     }
 
     // @dev Estimate only.
-    // @dev Only measures against strategy's current CRV balance, and will be inaccurate if period data is stale.
     function estimateSplitRatios() external view returns (uint ySplit, uint tSplit) {
         (ySplit, tSplit) = _computeSplitRatios();
     }
@@ -174,17 +172,13 @@ contract Splitter {
         strategy = _strategy;
     }
 
-
+    // @notice For use by yearn only to update discretionary values
+    // @dev Other values in the struct are either immutable or require agreement by both parties to update.
     function setYearn(address _recipient, uint _keepCRV) external {
         require(msg.sender == yearn.admin);
         require(_keepCRV <= 10_000, "!tooHigh");
-        yearn = Yearn(
-            _recipient,
-            yearn.voter, // Cannot update this value
-            yearn.admin,
-            yearn.share, // Cannot update this value
-            _keepCRV
-        );
+        yearn.recipient = _recipient;
+        yearn.keepCRV = _keepCRV;
     }
 
     function setTemple(address _recipient) external {
@@ -194,8 +188,7 @@ contract Splitter {
 
     // @notice update share if both parties agree.
     function updateYearnShare(uint _share) external {
-        // Disallow 0 setting for safety
-        require(_share <= 10_000 && _share != 0, "!outOfRange");
+        require(_share <= 10_000 && _share != 0, "OutOfRange");
         require(msg.sender == yearn.admin || msg.sender == templeRecipient);
         if(msg.sender == yearn.admin){
             pendingShare[msg.sender] = _share;
@@ -203,7 +196,7 @@ contract Splitter {
                 yearn.share = _share;
             }
         }
-        if(msg.sender == templeRecipient){
+        else if(msg.sender == templeRecipient){
             pendingShare[msg.sender] = _share;
             if (pendingShare[yearn.admin] == _share) {
                 yearn.share = _share;
