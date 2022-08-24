9fcb4ff0c32856fcb4cadf7b4075c75da78db4a3
dudesahn <dudesahn@gmail.com>
2022-08-24 08:47:49 -0400

    chore: final touches

--- tests/test_setters.py
+++ tests/test_setters.py
@@ -69,7 +69,10 @@ def test_setters(
         strategy.setHarvestTriggerParams(90000e6, 150000e6, 1e24, False, {"from": gov})
     else:
         strategy.setKeepCRV(0, {"from": gov})
-    strategy.setUniFees(3000, {"from": gov})
+    try:
+        strategy.setUniFees(3000, {"from": gov})
+    except:
+        print("\nThis strategy doesn't have Uniswap fees, most likely ETH-based")
 
     strategy.setStrategist(strategist, {"from": gov})
     name = strategy.name()
